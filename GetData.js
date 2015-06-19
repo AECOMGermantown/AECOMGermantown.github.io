84223126ae5833175035f8b092a5763f6617aafe
yseah <yseah@myway.com>
2015-06-19 10:15:11 -0400

    second changes to getdata.js

--- GetData.js
+++ GetData.js
@@ -1,2 +1,19 @@
-function fetchData(type) {
+function fetchData(btnType) {
+    alert("hi2");
+    var arr = new Array();
+    $.ajax({ // ajax call starts
+        url: 'http://api.fda.gov/' + btnType + '/enforcement.json?&limit=' + perBatchAmount + '&skip=' + currentCount,
+        dataType: 'json' // Choosing a JSON datatype
+    })
+    .fail(function () {
+        alert("Ajax failed to fetch data in fetchData");
+    })
+    .done(function (data) { // Variable data contains the data we get from serverside
+        debugger;
+        for (var j in data.results) {
+            arr.push(data.results[j].recall_number, data.results[j].reason_for_recall, data.results[j].status, data.results[j].recall_initiation_date);
+            $('#listInfo').append(data.results[j].recall_number + '<br/>');
+        }
+    });
+
 }
